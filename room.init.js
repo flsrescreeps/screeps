08ff1e8a0606acaea58a010d118cbd1370e2aef6
Kyle Goedtel <kgoedtel@linkedin.com>
2017-09-27 18:57:25 -0700

    more foundation work

--- room.init.js
+++ room.init.js
@@ -41,7 +41,7 @@ let roomInit = {
             currentRoom.memory.exits = exits;
         }
 
-        // Sources Data (+ creeps.miners and creeps.movers)
+        // Sources Data (+creeps)
         let sourcesData = currentRoom.lookForAtArea(LOOK_SOURCES, 0, 0, 49, 49, true);
         let sources = [];
         let creeps = CREEPS;
@@ -75,7 +75,6 @@ let roomInit = {
                 currentRoom.memory.controller = {
                     x: structuresData[structure].x,
                     y: structuresData[structure].y,
-                    // level: structuresData[structure].structure.level
                 };
             }
         }
